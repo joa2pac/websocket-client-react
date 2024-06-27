0d8a8e12a82551b27dd88fc06af508de91403862
joaquin <joaquinahuel93@gmail.com>
2024-06-27 18:23:27 -0300

    update

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,11 @@ import './style.css'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
-   <h1>Websocket - Client</h1>
+   <h2>Websocket - Client</h2>
+   <input id="jwt-token" placeholder="Json Web Token" />
+   <button id="btn-connect">Connect</button>
 
+   <br/>
    <span id="server-status">offline</span>
 
    <ul id="clients-ul">
@@ -21,4 +24,12 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
 `
 // setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
 
-connectToServer();
+const jwtToken = document.querySelector<HTMLInputElement>('#jwt-token')!;
+const btnConnect = document.querySelector<HTMLButtonElement>('#btn-connect')!;
+
+btnConnect.addEventListener('click', () => {
+
+  if( jwtToken.value.trim().length <= 0 ) return alert('Enter a valid JWT');
+
+  connectToServer( jwtToken.value );
+});
